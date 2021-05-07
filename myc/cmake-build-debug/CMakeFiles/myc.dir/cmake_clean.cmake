e422db9c17f0800e424d8461cb31809b208ad33e
liwei <wei.li@zenjoy.net>
2021-05-07 16:48:54 +0800

    [LW] my c test

---
+++ myc/cmake-build-debug/CMakeFiles/myc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myc.dir/main.c.o"
+  "myc"
+  "myc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ myc/cmake-build-debug/CMakeFiles/myc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for myc.
+# This may be replaced when dependencies are built.
