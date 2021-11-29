aef965f7ea994b82ccaac20264772317c5e8b246
liwei <wei.li@zenjoy.net>
2021-11-29 19:22:55 +0800

    [LW]feat: add Todo cc

---
+++ TodoCC/cmake-build-debug/CMakeFiles/TodoCC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TodoCC.dir/main.cpp.o"
+  "TodoCC"
+  "TodoCC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TodoCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TodoCC/cmake-build-debug/CMakeFiles/TodoCC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TodoCC.
+# This may be replaced when dependencies are built.
